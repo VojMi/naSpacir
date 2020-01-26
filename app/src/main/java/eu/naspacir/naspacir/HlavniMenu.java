225c06f6320ee0ca9e17e72ef7f144f9b0822fea
vojmi <vojmi@gmail.com>
2020-01-27 00:33:39 +0100

    Koncept UI

--- app/src/main/java/eu/naspacir/naspacir/HlavniMenu.java
+++ app/src/main/java/eu/naspacir/naspacir/HlavniMenu.java
@@ -31,6 +31,42 @@ public class HlavniMenu extends AppCompatActivity {
 
             }
         });
+
+        final Button btn_bolistky = findViewById(R.id.bolistky);
+        btn_bolistky.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Zalozky.class);
+                startActivity(intent);
+
+            }
+        });
+
+        final Button btn_vestba = findViewById(R.id.vestba);
+        btn_vestba.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Zalozky.class);
+                startActivity(intent);
+
+            }
+        });
+
+        final Button btn_halusky = findViewById(R.id.halusky);
+        btn_halusky.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Zalozky.class);
+                startActivity(intent);
+
+            }
+        });
+
+        final Button btn_vzdat = findViewById(R.id.vzdat);
+        btn_vzdat.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Zalozky.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
